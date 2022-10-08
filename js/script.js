a248e4158482d128984a11cc172e7e317a339baf
KestPetrenko <KestPetrenko@gmail.com>
2022-10-08 19:58:06 +0300

    push sms

--- js/script.js
+++ js/script.js
@@ -144,7 +144,11 @@ function startTime() {
     }
     if (min >= 0) {
         minuts.innerHTML = print(min) + `:`;
-    } else {
+    }
+    if(min==1){
+        notifSet ();
+    }
+    else {
         min = 0;
         minuts.innerHTML = print(min) + `:`;
     }
@@ -163,5 +167,28 @@ function print(e) {
         return "0" + e;
     } else {
         return e;
+    }
+}
+
+function notifyMe () {
+    let notification = new Notification ("Все еще работаешь?", {
+        tag : "ache-mail",
+        body : "Пора сделать паузу и отдохнуть",
+        icon : "https://itproger.com/img/notify.png"
+    });
+}
+
+function notifSet () {
+    if (!("Notification" in window))
+        alert ("Ваш браузер не поддерживает уведомления.");
+    else if (Notification.permission === "granted")
+        setTimeout(notifyMe, 2000);
+    else if (Notification.permission !== "denied") {
+        Notification.requestPermission (function (permission) {
+            if (!('permission' in Notification))
+                Notification.permission = permission;
+            if (permission === "granted")
+                setTimeout(notifyMe, 2000);
+        });
     }
 }
